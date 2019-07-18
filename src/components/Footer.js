b54ec1277b22222914181602e1054dd25e7edfc1
John Fry <john@sixheads.com>
2019-07-18 15:42:33 +1000

    Add separate Footer comp

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      &copy; {new Date().getFullYear()} John Fry. All Rights Reserved.
+    </footer>
+  )
+}
+
+export default Footer
