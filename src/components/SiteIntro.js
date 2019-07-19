78326c54379193bf4d207e6c22db219b74a2911d
John Fry <john@sixheads.com>
2019-07-19 14:23:44 +1000

    Add CTA to SiteIntro

--- src/components/SiteIntro.js
+++ src/components/SiteIntro.js
@@ -1,8 +1,15 @@
 import React from "react"
+import Link from "gatsby-link"
+import PageTransition from 'gatsby-plugin-page-transitions'
 
-const SiteIntro = ({intro}) => (
+const SiteIntro = ({ intro, cta }) => (
   <div className="site__intro">
-    <p>{intro}</p>
+    <PageTransition transitionTime={500}>
+      <p>{intro}</p>
+      <p className="site__intro-cta">
+        <Link to="/contact/">{cta}</Link>
+      </p>
+    </PageTransition>
   </div>
 )
 
